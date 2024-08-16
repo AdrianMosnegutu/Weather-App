857c80b9669c6afac51e47bc7e685d4bf07c8699
Moșneguțu Adrian-Ioan <146835839+MosnegutuAdrian@users.noreply.github.com>
2024-08-16 21:20:24 +0300

    style temperature display

--- src/components/CurrentWeather/TemperatureDisplay.jsx
+++ src/components/CurrentWeather/TemperatureDisplay.jsx
@@ -3,14 +3,18 @@ import { CurrentWeatherContext } from "../../containers/CurrentWeather";
 
 const TemperatureDisplay = () => {
   const { temp, tempMin, tempMax } = useContext(CurrentWeatherContext);
+  let percentage = Math.floor(((temp - tempMin) / (tempMax - tempMin)) * 100);
 
   return (
-    <div>
-      <span>{tempMin}°C</span>
-      <div>
-        <div />
+    <div className="w-fill flex h-9 items-center gap-5">
+      <span className="text-2xl">{tempMin}°C</span>
+      <div className="flex h-5 w-full items-center justify-start rounded-full bg-white">
+        <div
+          style={{ width: `${percentage}%` }}
+          className={`bg-accent h-full rounded-full`}
+        />
       </div>
-      <span>{tempMax}°C</span>
+      <span className="text-2xl">{tempMax}°C</span>
     </div>
   );
 };
