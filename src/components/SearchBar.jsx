b22ff57e5b60da33e5cac43c5484a3f815a02c12
Moșneguțu Adrian-Ioan <146835839+MosnegutuAdrian@users.noreply.github.com>
2024-08-16 19:39:29 +0300

    style search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,15 +11,19 @@ const SearchBar = ({ onLocationChange }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className="flex h-12 items-center" onSubmit={handleSubmit}>
       <input
+        className="h-full w-full rounded-l-full bg-white px-5 shadow-inner"
         type="text"
         placeholder="Insert location..."
         value={userInput}
         onChange={(e) => setUserInput(e.target.value)}
       />
-      <button type="submit">
-        <FaSearch />
+      <button
+        className="bg-accent h-full rounded-r-full px-6 text-white transition-all ease-linear hover:bg-opacity-75 active:bg-opacity-50"
+        type="submit"
+      >
+        <FaSearch size={20} />
       </button>
     </form>
   );
