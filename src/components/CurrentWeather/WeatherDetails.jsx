fee2746cd614e829ab793a07fa7d26f37a254c6c
Moșneguțu Adrian-Ioan <146835839+MosnegutuAdrian@users.noreply.github.com>
2024-08-16 21:26:52 +0300

    style weather details

--- src/components/CurrentWeather/WeatherDetails.jsx
+++ src/components/CurrentWeather/WeatherDetails.jsx
@@ -15,46 +15,44 @@ const WeatherDetails = () => {
   const data = useContext(CurrentWeatherContext);
 
   return (
-    <ul>
+    <ul className="flex flex-col gap-7">
       <WeatherProperty
         name="Description"
-        icon={<FaInfoCircle />}
+        icon={<FaInfoCircle size={20} />}
         value={data.description}
       />
-      <li>
-        <ul>
+      <li className="flex justify-between">
+        <ul className="flex flex-col gap-7">
           <WeatherProperty
             name="Feels Like"
-            icon={<FaThermometerHalf />}
+            icon={<FaThermometerHalf size={20} />}
             value={`${data.feelsLike}°C`}
           />
           <WeatherProperty
             name="Humidity"
-            icon={<FaDroplet />}
+            icon={<FaDroplet size={20} />}
             value={`${data.humidity}%`}
           />
           <WeatherProperty
             name="Visibility"
-            icon={<FaBinoculars />}
+            icon={<FaBinoculars size={20} />}
             value={`${data.visibility / 1000} km`}
           />
         </ul>
-      </li>
-      <li>
-        <ul>
+        <ul className="flex flex-col gap-7">
           <WeatherProperty
             name="Wind Speed"
-            icon={<FaWind />}
+            icon={<FaWind size={20} />}
             value={`${data.windSpeed} m/s`}
           />
           <WeatherProperty
             name="Wind Direction"
-            icon={<FaCompass />}
+            icon={<FaCompass size={20} />}
             value={`${data.windDirection}°`}
           />
           <WeatherProperty
             name="Pressure"
-            icon={<FaTachometerAlt />}
+            icon={<FaTachometerAlt size={20} />}
             value={`${data.pressure} hPa`}
           />
         </ul>
