c2f860b535ecc3306362fb7943fc5f149a4c4773
Moșneguțu Adrian-Ioan <146835839+MosnegutuAdrian@users.noreply.github.com>
2024-08-16 19:55:22 +0300

    add weather properties

---
+++ src/components/WeatherProperties.jsx
@@ -0,0 +1,66 @@
+import { useContext } from "react";
+import {
+  FaBinoculars,
+  FaCompass,
+  FaInfoCircle,
+  FaTachometerAlt,
+  FaThermometerHalf,
+  FaWind,
+} from "react-icons/fa";
+import { FaDroplet } from "react-icons/fa6";
+import { CurrentWeatherContext } from "../containers/CurrentWeather";
+import WeatherProperty from "./WeatherProperty";
+
+const WeatherProperties = () => {
+  const data = useContext(CurrentWeatherContext);
+
+  return (
+    <ul>
+      <WeatherProperty
+        name="Description"
+        icon={<FaInfoCircle />}
+        value={data.description}
+      />
+      <li>
+        <ul>
+          <WeatherProperty
+            name="Feels Like"
+            icon={<FaThermometerHalf />}
+            value={`${data.feelsLike}°C`}
+          />
+          <WeatherProperty
+            name="Humidity"
+            icon={<FaDroplet />}
+            value={`${data.humidity}%`}
+          />
+          <WeatherProperty
+            name="Visibility"
+            icon={<FaBinoculars />}
+            value={`${data.visibility / 1000} km`}
+          />
+        </ul>
+      </li>
+      <li>
+        <ul>
+          <WeatherProperty
+            name="Wind Speed"
+            icon={<FaWind />}
+            value={`${data.windSpeed} m/s`}
+          />
+          <WeatherProperty
+            name="Wind Direction"
+            icon={<FaCompass />}
+            value={`${data.windDirection}°`}
+          />
+          <WeatherProperty
+            name="Pressure"
+            icon={<FaTachometerAlt />}
+            value={`${data.pressure} hPa`}
+          />
+        </ul>
+      </li>
+    </ul>
+  );
+};
+
+export default WeatherProperties;
